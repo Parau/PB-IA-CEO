dffa491e370dad5fc8c6b930658a93cf0baf9f50
APRENDER.digital <info@aprender.digital>
2024-05-04 16:07:32 -0300

    inclusão de ajustes no mermaid

--- src/components/mermaid-diagram.js
+++ src/components/mermaid-diagram.js
@@ -1,9 +1,12 @@
 export default `
 flowchart TD
-    A[Apresentação] --> B(O desafio)
-    B --> C{Let me think}
+    classDef concluido fill:#f9f,stroke:#333,stroke-width:1px;
+
+    A(fa:fa-info-circle Apresentação):::concluido --> B(fa:fa-mountain O desafio)
+    B --> C(fa:fa-tasks Casos de uso)
     C -->|One| D[Laptop]
     C -->|Two| E[iPhone]
-    C -->|Three| F[fa:fa-car Car]
-    
+    C -->|/icons/| F[fa:fa-car Car]
+    click A "docs/intro"
+    click B "docs/category/o-desafio"
     `;
